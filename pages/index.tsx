84baab34a14b83a8aefc14c700a36ac83184451e
Mhd Rizki Purba <5422573-muhammadrizkipurba@users.noreply.gitlab.com>
2023-01-24 14:39:34 +0700

    add loading state on home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,23 @@
-import Layout from "../components/layout/index";
-import AnimeLists from "@/components/ui/animeLists";
 import axios from "axios";
 import { NextPage } from 'next';
 import { useState, useEffect } from 'react';
 
+import Layout from "@/components/layout/index";
+import AnimeLists from "@/components/ui/animeLists";
+
 const Home: NextPage = () => {
+  const [loading, setLoading] = useState<boolean>(false);
   const [popularAnimes, setPopularAnimes] = useState<AnimeData[] | []>([]);
   
   useEffect(() => {
+    setLoading(true);
     axios.get(`https://api.jikan.moe/v4/anime?page=1&limit=8&order_by=popularity`)
     .then(response => {
       setPopularAnimes(response.data.data)
+      setLoading(false);
     }).catch(err => {
       console.log(err)
+      setLoading(false);
     })
   
     return () => {}
@@ -20,7 +25,13 @@ const Home: NextPage = () => {
   
   return (
     <Layout>
-      <AnimeLists popularAnimes={popularAnimes} />
+      { loading ? 
+        <div className="h-[50vh] w-full flex flex-col justify-center items-center overflow-hidden bg-white left-0 z-[60]" >
+          <div className="loadingSpinner"></div>
+          <p className="text-lg">Please wait...</p>
+        </div>
+        : <AnimeLists popularAnimes={popularAnimes} />
+      }
     </Layout>
   );
 };
